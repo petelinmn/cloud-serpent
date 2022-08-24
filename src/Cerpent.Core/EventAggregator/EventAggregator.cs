57a0fd9a0874641e35910f511345763f477d72d3
Petelin Maksim <maksim.petelin@happify.com>
2022-08-24 14:36:38 -0400

    a lot of work

--- src/Cerpent.Core/EventAggregator/EventAggregator.cs
+++ src/Cerpent.Core/EventAggregator/EventAggregator.cs
@@ -1,7 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using Cerpent.Core.Contract;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Cerpent.Core.EventAggregator;
+namespace Cerpent.Core;
 
 public class EventAggregator
 {
@@ -14,39 +15,44 @@ public class EventAggregator
         AggregationRuleSource = aggregationRuleSource;
     }
 
-    private IAggregationRule[] GetRules(string triggerEventName) =>
-        AggregationRuleSource.Get(triggerEventName)
-            .Where(rule => rule.Atomics?.ContainsKey(triggerEventName) == null)
+    private async Task<AggregationRule[]> GetRules(string triggerEventName) =>
+        (await AggregationRuleSource.Get(triggerEventName))
+            .Where(rule => rule.Atomics?.ContainsKey(triggerEventName) == true)
             .ToArray();
 
-    public IEnumerable<Event> Aggregate(Event triggerEvent)
+    public async Task<IEnumerable<Event>> Aggregate(Event triggerAtomicEvent)
     {
-        var rules = GetRules(triggerEvent.Name);
+        var rules = await GetRules(triggerAtomicEvent.Name);
 
-        var dataDictionary = triggerEvent.Data != null
-            ? JsonConvert.DeserializeObject<JObject>(triggerEvent.Data)
-            : null;
+        var dataDictionary = triggerAtomicEvent.Data;
 
-        var contextDictionary = dataDictionary is null
-            ? null
-            : rules
-                .Select(rule => rule.ContextFields)
-                .SelectMany(contextFields => contextFields)
-                .Distinct()
-                .Where(field => rules.All(rule => rule.ContextFields.Contains(field)))
-                .ToDictionary(field => field, field => dataDictionary[field]);
+        var contextDictionary = rules
+            .Select(rule => rule.ContextFields)
+            .SelectMany(contextFields => contextFields)
+            .Distinct()
+            .Where(field => rules.All(rule => rule.ContextFields.Contains(field)))
+            .ToDictionary(field => field, field => dataDictionary[field]);
 
         var atomicEvents = rules
             .Select(rule => rule.Atomics)
             .SelectMany(queue => queue)
-            .Distinct();
+            .Distinct()
+            .Select(a => a.Key)
+            .ToArray();
 
-        var timeSpan = rules.MaxBy(rule => rule.TimeSpanInSec)?.TimeSpanInSec ?? 3600;
-        var eventList = EventSource.Get(atomicEvents.Select(a => a.Key),
-            contextDictionary, timeSpan).ToArray();
+        var timeSpan = rules.MaxBy(rule => rule.TimeSpan)?.TimeSpan ?? 3600;
+        var eventList = (await EventSource.Get(atomicEvents,
+            contextDictionary, timeSpan)).ToList();
 
-        if (eventList == null || !eventList.Any())
-            return Array.Empty<Event>();
+        eventList = eventList
+            .Where(e => contextDictionary
+                .All(c => e.Data[c.Key]?.ToString() == c.Value?.ToString()))
+            .ToList();
+        
+        if (eventList.All(e => e.Id != triggerAtomicEvent.Id))
+            eventList.Add(triggerAtomicEvent);
+
+        eventList = eventList.OrderByDescending(o => o.DateTime).ToList();
 
         var newEvents = new List<Event>();
         foreach (var rule in rules)
@@ -54,24 +60,23 @@ public class EventAggregator
             var ruleEvents = eventList
                 .Where(e => rule.Atomics?.ContainsKey(e.Name) == true)
                 .ToArray();
-
+            
             if (rule.Atomics.Any(atomic =>
                     ruleEvents.Count(ruleEvent => ruleEvent.Name == atomic.Key) < atomic.Value))
                 continue;
 
             var newEventData = rule.Atomics.ToDictionary(atomic => atomic.Key,
                 atomic => ruleEvents
-                    .Where(ruleEvent => ruleEvent.Name == atomic.Key)
-                    .Select(ruleEvent => ruleEvent?.Data is null
-                        ? null
-                        : JsonConvert.DeserializeObject<JObject>(ruleEvent.Data))
+                    .Where(ruleEvent => ruleEvent.Name == atomic.Key).Take(atomic.Value)
+                    .Select(ruleEvent => ruleEvent?.Data)
                     .ToArray());
 
             newEvents.Add(new Event()
             {
+                Id = Guid.NewGuid(),
                 Name = rule.Name,
                 DateTime = DateTime.Now,
-                Data = JsonConvert.SerializeObject(newEventData)
+                Data = JToken.FromObject(newEventData)
             });
         }
 
